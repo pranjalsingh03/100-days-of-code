aa77f7141c9281f31568528a2c7b7d4fd2a73da6
Pranjal-singh <pranjalsingh9304@gmail.com>
2023-04-03 20:42:47 +0530

    inital changes

--- Week_1/day2.java
+++ Week_1/day2.java
@@ -1,10 +1,14 @@
+package Week_1;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
-class learning{
-    public static void main(String args[]){
+public class day2{
+    public static void main(String[] args){
         JFrame frame = new JFrame("My First GUI");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(300, 300);
         frame.setVisible(true);
+        
     }
 }

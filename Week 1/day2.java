8d1c88b5056194bab876f82045030cc671e9f4cc
Pranjal-singh <pranjalsingh9304@gmail.com>
2023-04-03 20:27:26 +0530

    day 2 april 3

---
+++ Week 1/day2.java
@@ -0,0 +1,10 @@
+import javax.swing.JFrame;
+
+class learning{
+    public static void main(String args[]){
+        JFrame frame = new JFrame("My First GUI");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300, 300);
+        frame.setVisible(true);
+    }
+}

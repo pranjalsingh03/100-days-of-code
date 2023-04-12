90dbfa59d72e4ce9786963353c2a7053bd4e615e
Pranjal-singh <pranjalsingh9304@gmail.com>
2023-04-12 23:22:09 +0530

    functions update

--- Week_2/day3.c
+++ Week_2/day3.c
@@ -8,3 +8,25 @@ struct taxpayer {
     float income;
     float tax;
 };
+
+struct taxpayer taxPayers[MAX_TAXPAYERS];
+int numTaxPayers = 0;
+
+void addRecord() {
+    struct taxpayer tp;
+    printf("\nEnter taxpayer name: ");
+    scanf("%s", tp.name);
+    printf("Enter taxpayer income: ");
+    scanf("%f", &tp.income);
+    if (tp.income <= 250000) {
+        tp.tax = 0;
+    } else if (tp.income > 250000 && tp.income <= 500000) {
+        tp.tax = (tp.income - 250000) * 0.05;
+    } else if (tp.income > 500000 && tp.income <= 1000000) {
+        tp.tax = 12500 + (tp.income - 500000) * 0.2;
+    } else {
+        tp.tax = 112500 + (tp.income - 1000000) * 0.3;
+    }
+    taxPayers[numTaxPayers++] = tp;
+    printf("Record added successfully.\n");
+}

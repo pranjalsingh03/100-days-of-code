e39e6406163c5068fef6e462b77c3af5bd27e315
Pranjal-singh <pranjalsingh9304@gmail.com>
2023-04-13 21:05:32 +0530

    online banking system in c language

--- Week_2/day3.c
+++ Week_2/day3.c
@@ -3,7 +3,8 @@
 
 #define MAX_TAXPAYERS 100
 
-struct taxpayer {
+struct taxpayer
+{
     char name[50];
     float income;
     float tax;
@@ -12,27 +13,36 @@ struct taxpayer {
 struct taxpayer taxPayers[MAX_TAXPAYERS];
 int numTaxPayers = 0;
 
-void addRecord() {
+void addRecord()
+{
     struct taxpayer tp;
     printf("\nEnter taxpayer name: ");
     scanf("%s", tp.name);
     printf("Enter taxpayer income: ");
     scanf("%f", &tp.income);
-    if (tp.income <= 250000) {
+    if (tp.income <= 250000)
+    {
         tp.tax = 0;
-    } else if (tp.income > 250000 && tp.income <= 500000) {
+    }
+    else if (tp.income > 250000 && tp.income <= 500000)
+    {
         tp.tax = (tp.income - 250000) * 0.05;
-    } else if (tp.income > 500000 && tp.income <= 1000000) {
+    }
+    else if (tp.income > 500000 && tp.income <= 1000000)
+    {
         tp.tax = 12500 + (tp.income - 500000) * 0.2;
-    } else {
+    }
+    else
+    {
         tp.tax = 112500 + (tp.income - 1000000) * 0.3;
     }
     taxPayers[numTaxPayers++] = tp;
     printf("Record added successfully.\n");
-
-    
-}void listRecords() {
-    if (numTaxPayers == 0) {
+}
+void listRecords()
+{
+    if (numTaxPayers == 0)
+    {
         printf("\nNo records found.\n");
         return;
     }
@@ -40,8 +50,65 @@ void addRecord() {
     printf("--------------------------------------------------\n");
     printf("%-20s %-20s %-20s\n", "Name", "Income", "Income Tax");
     printf("--------------------------------------------------\n");
-    for (int i = 0; i < numTaxPayers; i++) {
+    for (int i = 0; i < numTaxPayers; i++)
+    {
         struct taxpayer tp = taxPayers[i];
         printf("%-20s %-20.2f %-20.2f\n", tp.name, tp.income, tp.tax);
     }
 }
+
+void searchRecord()
+{
+    char name[50];
+    printf("\nEnter name to search: ");
+    scanf("%s", name);
+    for (int i = 0; i < numTaxPayers; i++)
+    {
+        struct taxpayer tp = taxPayers[i];
+        if (strcmp(tp.name, name) == 0)
+        {
+            printf("\nRecord found:\n");
+            printf("--------------------------------------------------\n");
+            printf("%-20s %-20s %-20s\n", "Name", "Income", "Income Tax");
+            printf("--------------------------------------------------\n");
+            printf("%-20s %-20.2f %-20.2f\n", tp.name, tp.income, tp.tax);
+            return;
+        }
+    }
+    printf("\nRecord not found.\n");
+}
+
+void editRecord()
+{
+    char name[50];
+    printf("\nEnter name to edit: ");
+    scanf("%s", name);
+    for (int i = 0; i < numTaxPayers; i++)
+    {
+        struct taxpayer tp = taxPayers[i];
+        if (strcmp(tp.name, name) == 0)
+        {
+            printf("\nEnter new income: ");
+            scanf("%f", &tp.income);
+            if (tp.income <= 250000)
+            {
+                tp.tax = 0;
+            }
+            else if (tp.income > 250000 && tp.income <= 500000)
+            {
+                tp.tax = (tp.income - 250000) * 0.05
+            }
+            else if (tp.income > 500000 && tp.income <= 1000000)
+            {
+                tp.tax = 12500 + (tp.income - 500000) * 0.2;
+            }
+            else
+            {
+                tp.tax = 112500 + (tp.income - 1000000) * 0.3;
+            }
+            taxPayers[i] = tp;
+            printf("\nRecord updated successfully.\n");
+            return;
+        }
+    }
+    printf("\nRecord not found.\n");

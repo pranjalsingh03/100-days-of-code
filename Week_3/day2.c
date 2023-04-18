e8272269953bb2141c6b2bd5d8815e3be5851c50
Pranjal-singh <pranjalsingh9304@gmail.com>
2023-04-18 17:59:35 +0530

    randome number generator

--- Week_3/day2.c
+++ Week_3/day2.c
@@ -1,2 +1,14 @@
 // Randome number generator
 
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    int num;
+    srand(time(0)); // Seed the random number generator with current time
+    num = rand(); // Generate a random number
+    printf("%d", num);
+    return 0;
+}

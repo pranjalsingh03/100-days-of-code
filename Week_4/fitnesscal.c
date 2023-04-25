337df2d7b7000fff94fd513a65db0bde2e9cf190
Pranjal-singh <pranjalsingh9304@gmail.com>
2023-04-25 23:20:12 +0530

    project calculator

---
+++ Week_4/fitnesscal.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+void vo2max_calculations(char, int, int, float, float);
+void details(char[50], int, char, float, float);
+int main()
+{
+char name[50],gender;
+int age,maximum_heart_rate,i=16,j=3,l=236,m=233,n=175,o=253;              ///i,j,l,m,n & o for ASCI characters
+float bmi,weight,height,minimum_heart_rate,vo2max;

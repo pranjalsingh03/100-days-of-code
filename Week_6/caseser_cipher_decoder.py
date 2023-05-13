c2bae6b7aa20e5d87b4c029c527da8dd7219ac7c
Pranjal-singh <pranjalsingh9304@gmail.com>
2023-05-14 00:51:08 +0530

    DECODER

--- Week_6/caseser_cipher_decoder.py
+++ Week_6/caseser_cipher_decoder.py
@@ -1,1 +1,19 @@
 #CASER CIPHER DECODER
+
+def main():
+    print("[>] CAESAR CIPHER DECODER!!! \n")
+    print("[1] Encrypt\n[2] Decrypt")
+    try:
+        func = int(input("Choose one of the above(example for encode enter 1): "))
+    except:
+        print("\n[>] Invalid input")
+        exit()
+
+    if func == 2:
+        decode()
+    else:
+        if func == 1:
+            encode()
+        else:
+            print("\n[>] Invalid input")
+        exit()

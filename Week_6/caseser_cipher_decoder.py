c538fa219e97a478e1704c259c6ffc0c9f42db4a
Pranjal-singh <pranjalsingh9304@gmail.com>
2023-05-14 00:51:35 +0530

    ciper decoder

--- Week_6/caseser_cipher_decoder.py
+++ Week_6/caseser_cipher_decoder.py
@@ -17,3 +17,35 @@ def main():
         else:
             print("\n[>] Invalid input")
         exit()
+
+def encode():
+    text = input("Enter text to encode: ")
+    key = input("Enter number of characters you want to shift: ")
+    encoded_cipher = ""
+    try:
+        key = int(key)
+    except:
+        print("Only intigers between 0 to 25 are allowed. Try again :)")
+        exit()
+    if key > 25:
+        print("Only intigers between 0 to 25 are allowed. Try again :)")
+        exit()
+    else:
+        key = key
+    text = text.upper()
+    for char in text:
+        ascii = ord(char)
+        if ascii > 90:
+            new_ascii = ascii
+        else:
+            if ascii < 65:
+                new_ascii = ascii
+            else:
+                new_ascii = ascii + key
+                if  new_ascii > 90:
+                    new_ascii = new_ascii - 26
+                else:
+                    new_ascii = new_ascii
+        encoded = chr(new_ascii)
+        encoded_cipher = encoded_cipher + encoded
+    print("Encoded text: " + encoded_cipher)

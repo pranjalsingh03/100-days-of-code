49c3a9b906df8fa7b6317522cea3ff99941f41ad
Pranjal-singh <pranjalsingh9304@gmail.com>
2023-06-17 19:45:11 +0530

    biutcoin wallet checker

--- Week_13/practice.c
+++ Week_13/practice.c
@@ -1,1 +1,9 @@
 #include<stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <openssl/sha.h>
+
+// Function to calculate SHA-256 hash
+void calculateHash(const char* data, unsigned char* hash) {
+    SHA256(data, strlen(data), hash);
+}
